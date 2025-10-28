356c9fd412036f2da3aadb18241b3f6861132cbc
Gaurav999-byte <nagralegaurav16@gmail.com>
2025-10-28 19:03:29 +0530

    Updated project: progress in week 20

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -19,6 +19,7 @@ import FavoritesCount from "../Products/FavoritesCount";
 
 const Navigation =() =>{
       const { userInfo } = useSelector((state) => state.auth);
+      const { cartItems } = useSelector((state) => state.cart);
 
       const [dropdownOpen, setDropdownOpen] = useState(false);
       const [showSidebar, setShowSidebar] = useState(false);
@@ -80,9 +81,22 @@ const Navigation =() =>{
           to="/cart"
           className="flex items-center transition-transform transform hover:translate-x-2"
         >
-          <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
+          <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26} />
           <span className="hidden nav-item-name mt-[3rem]">CART</span>{" "}
+
+          <div className="absolute top-9">
+            {cartItems.length > 0 && (
+              <span>
+                <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
+                  {cartItems.reduce((a, c) => a + c.qty, 0)}
+                </span>
+              </span>
+            )}
+          </div>
+          
         </Link>
+
+        
 
         <Link to="/favorite" className="flex relative">
           <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
